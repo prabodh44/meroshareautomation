b11ca829efd38b2ef4785bd7d94cfb7a07d033c2
prabodh44 <prabodhtuladhar44@gmail.com>
2023-01-09 14:47:54 +0545

    refined code to catch null nodes

--- scrapper.py
+++ scrapper.py
@@ -3,9 +3,6 @@ from helium import *
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 import pandas as pd
-import re
-import requests
-import json
 
 URL = "https://www.merorojgari.com/"
 # searchTerm = input("enter the search term ")
@@ -27,6 +24,7 @@ write(searchLocation, into=locationInput)
 click(Button('Search Jobs'))
 
 sleep(3) # wait 3 seconds before scraping
+
 
 
 DOMTree = driver.execute_script("return document.body.outerHTML;")
@@ -71,54 +69,29 @@ for job in jobList:
         except:
             jobStatus = 'N/A'
             jobDeadline = 'N/A'
-
-
-
+      
         df = df.append({'Job Names' : jobName,
-                'Job Companies' : jobCompany,
-                'Job Type': jobType,
-                'Job Link': link,
-                'Job Status' : jobStatus,
-                'Job Posted Date': jobPostedDate,
-                'Job Deadline': jobDeadline}, ignore_index=True)
+            'Job Companies' : jobCompany,
+            'Job Type': jobType,
+            'Job Link': link,
+            'Job Status' : jobStatus,
+            'Job Posted Date': jobPostedDate,
+            'Job Deadline': jobDeadline}, ignore_index=True)
 
-        
-
-
-# jobNames = [jobName.string for jobName in jobList.find_all('h3')]
-# jobCompanies = [jobCompany.string for jobCompany in jobList.find_all('strong')]
-# # added to class full-time to display only full time jobs and filter out fresher jobs
-# jobTypes = [jobType.string for jobType in jobList.find_all('li', class_='job-type full-time')]
-# jobPostedDates = [jobPostedDate['datetime'] for jobPostedDate in jobList.find_all('time')]
-# jobLinks = [jobLink['href'] for jobLink in jobList.find_all('a')]
+try:
+      loadMore = soup.find('a', class_="load_more_jobs")
+      click(Link('Load more listings'))
+      print('load more button found')
+except:
+      print('load more button not found')
 
 
-
-
-
-
-
-#  deadlines = ["None" if deadline.find('label').nextSibling is None else deadline.find('label').nextSibling for deadline in jobList.find_all('li', class_='application-deadline expiring expired')]
-# # print(deadlines)
-
-# create a dataframe to store the data
-
-
-print(df)
 
 #create an excel file to put the data
 df.to_excel('webscrapper.xlsx')
 
 
-
-
-
-
-
-
-
-
-# kill_browser()
+kill_browser()
 
 
 
